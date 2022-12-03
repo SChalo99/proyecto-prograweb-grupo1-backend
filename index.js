f8b58077b39816f9641548530e19da7c9bd11e26
SChalo99 <73367453+SChalo99@users.noreply.github.com>
2022-12-03 07:45:01 -0500

    port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function main() {
         
         console.log('Connection successful')
         
-        var PORT = 3001
+        var PORT = 3001 || process.env.PORT
 
         app.listen(PORT)
 
