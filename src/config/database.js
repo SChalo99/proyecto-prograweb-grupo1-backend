b332f2bc2cd2009dbfd5b75c9ed38fe311b1a68f
SChalo99 <73367453+SChalo99@users.noreply.github.com>
2022-12-03 07:13:00 -0500

    ENV variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize'
 
-const hostname = process.env.HOSTNAME || '127.0.0.1'
-const username = process.env.DBUSER || 'postgres'
-const password = process.env.PASSWORD || '123'
-const database = process.env.DATABASE || 'postgres'
+const hostname = process.env.PGHOST || '127.0.0.1'
+const username = process.env.PGUSER || 'postgres'
+const password = process.env.PGPASSWORD || '123'
+const database = process.env.PGDATABASE || 'postgres'
 const dialect = process.env.DIALECT || 'postgres'
-const port = process.env.DBPORT || 5432
+const port = process.env.PGPORT || 5432
 
 const sequelize = new Sequelize(database,username,password, {
     host: hostname,
